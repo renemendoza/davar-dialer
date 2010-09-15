f34f530ec27530def4e3bb1582f7f9a33fecc795
Rene Mendoza <latiendamac@gmail.com>
2010-09-15 14:27:07 -0500

    Contact List new&edit controller actions and specs

--- app/controllers/contact_lists_controller.rb
+++ app/controllers/contact_lists_controller.rb
@@ -1,4 +1,10 @@
 class ContactListsController < ApplicationController
+
+  def index
+    @contact_lists =  ContactList.find(:all)
+  end
+
   def new
+    @contact_list =  ContactList.new
   end
 end
--- spec/controllers/contact_lists_controller_spec.rb
+++ spec/controllers/contact_lists_controller_spec.rb
@@ -1,22 +1,75 @@
 require 'spec_helper'
 
 
-describe ListsController do
-  describe "GET /lists" do
+describe ContactListsController do
+
+  describe "GET /contact_lists" do
     before(:each) do 
-      #@list = Factory(:list)
-      @weather = mock_model(List)
+      @contact_list = Factory(:contact_list)
+      ContactList.stub!(:find).and_return([@contact_list])
     end
-    it "concats two strings with spaces" do
-  #       helper.concat_strings("this","that").should == "this that"
+
+    def do_get
+      get :index
     end
-   end
+
+    it "should be succesful" do
+      do_get
+      response.should be_success
+    end
+
+    it "should render the index template" do
+      do_get
+      response.should render_template("index")
+    end
+
+    it "should find all lists" do
+      ContactList.should_receive(:find).with(:all).and_return([@contact_list])
+      do_get
+    end
+
+    it "should assign all lists for the view" do
+      do_get
+      assigns[:contact_lists].should == [@contact_list]
+    end
+
+  end
+
+  describe "GET /contact_lists/new" do
+    before(:each) do 
+      @contact_list = Factory(:contact_list)
+      ContactList.stub!(:new).and_return(@contact_list)
+    end
+    
+    def do_get
+      get :new
+    end
+
+    it "should be succesful" do
+      do_get
+      response.should be_success
+    end
+
+    it "should render the new template" do
+      do_get
+      response.should render_template("new")
+    end
+
+    it "should create a new contact_list" do
+      ContactList.should_receive(:new).and_return(@contact_list)
+      do_get
+    end
+
+    it "should not save the new contact_list" do
+      @contact_list.should_not_receive(:save)
+      do_get
+    end
+
+    it "should assign the new list for the view" do
+      do_get
+      assigns[:contact_list].should == @contact_list
+    end
+
+  end
 end
 
-# describe ListsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
