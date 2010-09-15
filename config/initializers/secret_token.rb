b8e3c6e5b83cff147ea0802b89d3c7c0283d4794
Rene Mendoza <latiendamac@gmail.com>
2010-09-14 21:40:27 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DavarDialer::Application.config.secret_token = 'bf42ec8418f82a7acda65fc956fc127af499150bf59ed7f64a6980fa1fc784f38ab77673774a9f54339b588278a10f458fd89fbae5e91518c8398145018d8a3e'
