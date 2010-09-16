88882d74788c25d64540c9a8a3f7abf4528938ee
Rene Mendoza <latiendamac@gmail.com>
2010-09-16 14:06:08 -0500

    fixed contactlist uploaded file content type and presence validations

--- app/models/contact_list.rb
+++ app/models/contact_list.rb
@@ -1,3 +1,25 @@
 class ContactList < ActiveRecord::Base
   has_attached_file :list
+
+  has_many :contacts
+
+  validates_attachment_presence :list
+
+  validates_attachment_content_type :list, :content_type => ['text/csv','text/comma-separated-values','text/csv','application/csv','application/excel','application/vnd.ms-excel','application/vnd.msexcel','text/anytext','text/plain']
+
+  after_save :create_contacts
+
+  private
+  def create_contacts
+   # raise "#{self.list.queued_for_write[:original].inspect} "
+    #
+    # MOVE THIS TO RESQUE background_job style
+    
+     File.open(self.list.path).each do |line|
+      line.chomp!
+      name, phone_number_1 = line.split(",")
+      self.contacts << Contact.create(:name => name, :phone_number_1 => phone_number_1)
+     end 
+      
+  end
 end
