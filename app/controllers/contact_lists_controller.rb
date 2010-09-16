e21f0c74ce48affdd044f1ffc9e9067e5538d828
Rene Mendoza <latiendamac@gmail.com>
2010-09-16 15:32:06 -0500

    added contact list controller show action and show template view

--- app/controllers/contact_lists_controller.rb
+++ app/controllers/contact_lists_controller.rb
@@ -12,11 +12,16 @@ class ContactListsController < ApplicationController
   def create
     @contact_list =  ContactList.new(params[:contact_list])
     if @contact_list.save
+
       flash[:notice] = "Contact list created"
       redirect_to contact_lists_path
     else
       flash.now[:error] = "There was an error creating the requested contact list"
       render :action => 'new'
     end
+  end
+
+  def show
+    @contact_list =  ContactList.find(params[:id])
   end
 end
