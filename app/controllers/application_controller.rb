a08cdf3f0ea78b410e869a03b29651814cc677bf
Rene Mendoza <latiendamac@gmail.com>
2010-09-17 16:53:19 -0500

    added current_user methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,33 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_user, :current_user_session
+
+  private
+
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = Session.find
+  end
+
+  
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.record 
+  end
+
+
+  def require_valid_account
+    unless current_user
+      store_location
+      flash[:error] = "You must be logged in to access this page"
+      redirect_to login_path
+      return false
+    end
+  end
+
+  def store_location
+    #session[:return_to] = request.request_uri
+    session[:return_to] = request.fullpath
+  end
 end
