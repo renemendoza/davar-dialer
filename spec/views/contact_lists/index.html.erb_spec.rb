c4096cc031a3ef4db7310ce604db637f126a8a64
Rene Mendoza <latiendamac@gmail.com>
2010-09-18 19:11:26 -0500

    fixed small issue with contact_lists index view spec

--- spec/views/contact_lists/index.html.erb_spec.rb
+++ spec/views/contact_lists/index.html.erb_spec.rb
@@ -6,6 +6,8 @@ describe "/contact_lists/index" do
   before(:each) do
     @contact_lists = [Factory(:contact_list_with_uploaded_file)]
     @contact_lists << Factory(:contact_list_with_uploaded_file)
+    view.stub(:current_user).and_return(Factory(:agent))
+
     render
   end
 
@@ -25,5 +27,6 @@ describe "/contact_lists/index" do
 
   after(:each) do
     ContactList.delete_all
+    Agent.delete_all
   end
 end
