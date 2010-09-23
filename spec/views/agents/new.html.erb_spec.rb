f987dcbd533ea8a2038e667af1754145d2be5733
Rene Mendoza <latiendamac@gmail.com>
2010-09-23 15:34:08 -0500

    added admin, agent and visitor roles to '/agents/new' and /contact_lists/index' view specs

--- spec/views/agents/new.html.erb_spec.rb
+++ spec/views/agents/new.html.erb_spec.rb
@@ -1,60 +1,134 @@
 require 'spec_helper'
 
 describe "/agents/new" do
-  before(:each) do
-    #we are not logged in
-    @agent = Factory.build(:agent)
-    assigns[:agent] = @agent
-    render 
-  end
+  describe "as a logged in administrator" do
+    before(:each) do
+      activate_authlogic
+      @agent = Factory.build(:agent)
+      @admin = Factory(:agent, :admin => true)
+      @session = Session.create(@admin)
+      assigns[:agent] = @agent
+      view.stub(:current_user).and_return(@admin)
+      render 
+    end
 
-  it "should say 'Agent registration'" do
-    rendered.should contain('Agent registration')
-  end
+    it "should say 'New Agent'" do
+      rendered.should contain('New Agent')
+    end
 
-  it "should render the new agent form" do
-    rendered.should have_selector("form[method=post]", :action => agents_path) 
-  end
+    it "should render the new agent form" do
+      rendered.should have_selector("form[method=post]", :action => agents_path) 
+    end
 
-  it "should have a name field" do
-    rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
-      form.should have_selector("input[type=text]", :name => "agent[name]")
+    it "should have a name field" do
+      rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
+        form.should have_selector("input[type=text]", :name => "agent[name]")
+      end
+    end
+
+    it "should have an email address field" do
+      rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
+        form.should have_selector("input[type=text]", :name => "agent[email]")
+      end
+    end
+
+    it "should have a username field" do
+      rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
+        form.should have_selector("input[type=text]", :name => "agent[username]")
+      end
+    end
+
+    it "should have a password field" do
+      rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
+        form.should have_selector("input[type=password]", :name => "agent[password]")
+      end
+    end
+
+    it "should have a password confirmation field" do
+      rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
+        form.should have_selector("input[type=password]", :name => "agent[password_confirmation]")
+      end
+    end
+
+    it "should have a callback number field" do
+      rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
+        form.should have_selector("input[type=text]", :name => "agent[ring_to_destination]")
+      end
+    end
+
+    it "should have a register button" do
+      rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
+        form.should have_selector("button[type=submit]")
+      end
+    end
+
+    after(:each) do 
+      Agent.delete_all
     end
   end
 
-  it "should have an email address field" do
-    rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
-      form.should have_selector("input[type=text]", :name => "agent[email]")
+  describe "as a visitor" do
+    before(:each) do
+      #we are not logged in
+      @agent = Factory.build(:agent)
+      #@agent = Factory(:agent, :admin => true))
+      assigns[:agent] = @agent
+      view.stub(:current_user).and_return(nil)
+      render 
     end
-  end
 
-  it "should have a username field" do
-    rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
-      form.should have_selector("input[type=text]", :name => "agent[username]")
+    it "should say 'Agent registration'" do
+      rendered.should contain('Agent registration')
     end
-  end
 
-  it "should have a password field" do
-    rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
-      form.should have_selector("input[type=password]", :name => "agent[password]")
+    it "should render the new agent form" do
+      rendered.should have_selector("form[method=post]", :action => agents_path) 
     end
-  end
 
-  it "should have a password confirmation field" do
-    rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
-      form.should have_selector("input[type=password]", :name => "agent[password_confirmation]")
+    it "should have a name field" do
+      rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
+        form.should have_selector("input[type=text]", :name => "agent[name]")
+      end
     end
-  end
 
-  it "should have a callback number field" do
-    rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
-      form.should have_selector("input[type=text]", :name => "agent[ring_to_destination]")
+    it "should have an email address field" do
+      rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
+        form.should have_selector("input[type=text]", :name => "agent[email]")
+      end
     end
-  end
 
-  it "should have a register button" do
-    rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
-      form.should have_selector("button[type=submit]")
+    it "should have a username field" do
+      rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
+        form.should have_selector("input[type=text]", :name => "agent[username]")
+      end
+    end
+
+    it "should have a password field" do
+      rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
+        form.should have_selector("input[type=password]", :name => "agent[password]")
+      end
+    end
+
+    it "should have a password confirmation field" do
+      rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
+        form.should have_selector("input[type=password]", :name => "agent[password_confirmation]")
+      end
+    end
+
+    it "should have a callback number field" do
+      rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
+        form.should have_selector("input[type=text]", :name => "agent[ring_to_destination]")
+      end
+    end
+
+    it "should have a register button" do
+      rendered.should have_selector("form[method=post]", :action => agents_path) do |form|
+        form.should have_selector("button[type=submit]")
+      end
+    end
+
+    after(:each) do 
+      Agent.delete_all
     end
   end
 end
--- spec/views/contact_lists/index.html.erb_spec.rb
+++ spec/views/contact_lists/index.html.erb_spec.rb
@@ -1,24 +1,44 @@
 require 'spec_helper'
 
 include ContactListsHelper
-
 describe "/contact_lists/index" do
   before(:each) do
     @contact_lists = [Factory(:contact_list_with_uploaded_file)]
     @contact_lists << Factory(:contact_list_with_uploaded_file)
-    view.stub(:current_user).and_return(Factory(:agent))
-
-    render
+    #view.stub(:current_user).and_return(Factory(:agent, :admin => true))
+    #render
   end
 
+  describe "as an administrator" do
+    before(:each) do
+      view.stub(:current_user).and_return(Factory(:agent, :admin => true))
+      render
+    end
 
-  it "should include a link to add a new contact_list" do
-    rendered.should have_selector("a[href='#{new_contact_list_path}']") 
+    it "should include a link to add a new contact_list" do
+      rendered.should have_selector("a[href='#{new_contact_list_path}']") 
+    end
+
+    it "should include a show link for each contact_list" do
+      @contact_lists.each do |c|
+        rendered.should have_selector("a[href='#{contact_list_path(c)}']") 
+      end
+    end
   end
+  describe "as an agent" do
+    before(:each) do
+      view.stub(:current_user).and_return(Factory(:agent))
+      render
+    end
 
-  it "should include a show link for each contact_list" do
-    @contact_lists.each do |c|
-      rendered.should have_selector("a[href='#{contact_list_path(c)}']") 
+    it "should not include a link to add a new contact_list" do
+      rendered.should_not have_selector("a[href='#{new_contact_list_path}']") 
+    end
+
+    it "should include a show link for each contact_list" do
+      @contact_lists.each do |c|
+        rendered.should have_selector("a[href='#{contact_list_path(c)}']") 
+      end
     end
   end
 
@@ -26,4 +46,5 @@ describe "/contact_lists/index" do
     ContactList.delete_all
     Agent.delete_all
   end
+
 end
