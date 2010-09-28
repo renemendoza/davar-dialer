ee6a6eea7fc7a7cad4e5b6bba1474b1ceb1b38ad
Rene Mendoza <latiendamac@gmail.com>
2010-09-28 14:30:35 -0500

    added class_method assign_contacts. has_many assigned_contacts and has_many unassigned_contacts relationships

--- app/models/contact_list.rb
+++ app/models/contact_list.rb
@@ -4,11 +4,16 @@ class ContactList < ActiveRecord::Base
 
   has_many :contacts
 
+  has_many :unassigned_contacts, :class_name => "Contact", :conditions => "assigned_to is NULL"
+  has_many :assigned_contacts, :class_name => "Contact", :conditions => "assigned_to is NOT NULL"
+  #
+  accepts_nested_attributes_for :contacts  #?
+
   validates_attachment_presence :list
 
   validates_attachment_content_type :list, :content_type => ['text/csv','text/comma-separated-values','text/csv','application/csv','application/excel','application/vnd.ms-excel','application/vnd.msexcel','text/anytext','text/plain']
 
-  after_save :create_contacts
+  after_save :create_contacts 
 
   private
   def create_contacts
@@ -22,5 +27,18 @@ class ContactList < ActiveRecord::Base
       self.contacts << Contact.create(:name => name, :phone_number_1 => phone_number_1)
      end 
       
+  end
+
+  def self.assign_contacts(params)
+    @agent = Agent.find(params.delete(:assigned_to))
+    contacts = Contact.find(params.delete(:id))
+    i = 0
+    contacts.each do |contact|
+      contact.agent = @agent
+      if contact.save
+        i += 1
+      end
+    end 
+    return i
   end
 end
