e59051341979384a5696fd46c7ff958da6234038
Rene Mendoza <latiendamac@gmail.com>
2010-09-28 14:24:42 -0500

    added belongs_to Agent relationship and assigned? method to Contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,14 @@
 class Contact < ActiveRecord::Base
   belongs_to :contact_list
+  belongs_to :agent, :foreign_key => :assigned_to
+
   before_validation do
     self.phone_number_1 = phone_number_1.gsub(/[^0-9]/, "") if attribute_present?("phone_number_1")
   end
+  
+  def assigned?
+    ! agent.nil?  
+  end
+
 
 end
